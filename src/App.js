54f46e2fb9382db48dfe4599de0ac7c69374591e
Arturo Cullinane <artocully@users.noreply.github.com>
2021-10-25 20:40:54 +0100

    Add initial form

--- src/App.js
+++ src/App.js
@@ -3,25 +3,61 @@ import { Button, Form, Container, Header } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
+  const [formState, setFormState] = React.useState({
+    name: '',
+    age: '',
+    salary: '',
+    hobby: '',
+  });
+
+  const changeHandler = (e) => {
+    setFormState({ [e.target.name] : e.target.value })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <Container fluid className="container">
       <Header as='h2'>Google Sheets Form</Header>
-      <Form className="form">
+      <Form className="form" onSubmit={submitHandler}>
         <Form.Field>
           <label>Name</label>
-          <input placeholder='Enter your name' />
+          <input
+            type="text"
+            placeholder='Enter your name'
+            name="name"
+            value={formState.name}
+            onChange={changeHandler} />
         </Form.Field>
         <Form.Field>
           <label>Age</label>
-          <input placeholder='Enter your age' />
+          <input
+            type="number"
+            placeholder='Enter your age'
+            name="age"
+            value={formState.age}
+            onChange={changeHandler} />
         </Form.Field>
         <Form.Field>
           <label>Salary</label>
-          <input placeholder='Enter your salary' />
+          <input
+            type="number"
+            placeholder='Enter your salary'
+            name="salary"
+            value={formState.salary}
+            onChange={changeHandler} />
         </Form.Field>
         <Form.Field>
           <label>Hobby</label>
-          <input placeholder='Enter your hobby' />
+          <input
+            type="text"
+            placeholder='Enter your hobby'
+            name="hobby"
+            value={formState.hobby}
+            onChange={changeHandler} />
         </Form.Field>
 
         <Button color="blue" type='submit'>Submit</Button>
